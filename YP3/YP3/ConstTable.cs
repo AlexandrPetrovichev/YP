78426bbcea6f2f269e4bff5c6dfe7ad107915c18
AlexandrPetrovichev <53701137+AlexandrPetrovichev@users.noreply.github.com>
2020-05-27 22:08:11 +0700

    Add files via upload

---
+++ YP3/YP3/ConstTable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace YP3
+{
+    class ConstTable : SortedSet<string>
+    {
+        private int num;
+        public ConstTable(int num) { this.num = num; }
+        public bool ReadFrom(string fileName)
+        {
+            string[] literals = File.ReadAllLines(fileName);
+            if (literals.Length == 0)
+            {
+                return false;
+            }
+            foreach (var item in literals)
+            {
+                Add(item);
+            }
+            return true;
+        }
+        public int get_num(string elem)
+        {
+            return num; 
+        }
+
+    }
+}
